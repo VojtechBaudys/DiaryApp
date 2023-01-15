1f16912de5dc22fd87a1cc6af3641ae7ba085d13
VojtechBaudys <vojta@baudys.name>
2023-01-15 23:27:23 +0100

    feature/lambáda add, show

--- DiaryApp/Diary.cs
+++ DiaryApp/Diary.cs
@@ -1,66 +1,176 @@
 ﻿using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace DiaryApp;
 
 public class Diary
 {
 	public bool Run;
+	public int DaysDiff; // days difference between today and some date
 	public MenuController MenuController;
+	private const string Format = "MM/dd/yyyy";
 	internal Diary()
 	{
 		Run = true;
+		DaysDiff = 0;
 		MenuController = new MenuController();
 	}
 	public void Loop()
 	{
-		ShowWeek();
-
+		
 		while (Run)
 		{
-			// MenuController.MainMenu();
-			// switch (GetInput())
-			// {
-			// 	case "a": case "add":
-			// 		// AddRecord();
-			// 		break;
-			// 	case "e": case "edit":
-			// 		// EditRecord();
-			// 		break;
-			// 	case "d": case "delete":
-			// 		// DeleteRecord();
-			// 		break;
-			// 	case "l": case "leave":
-			// 		Run = false;
-			// 		break;
-			// }
+			Console.WriteLine("-----------DIAREK----------\n");
+			ShowWeek();
+			MenuController.MainMenu();
+			switch (GetInput())
+			{
+				case "n": case "next":
+					DaysDiff += 7;
+					break;
+				case "p": case "prev":
+					DaysDiff -= 7;
+					break;
+				case "a": case "add":
+					AddTask();
+					break;
+				case "e": case "edit":
+					// EditTask();
+					break;
+				case "d": case "delete":
+					// DeleteTask();
+					break;
+				case "l": case "leave":
+					Run = false;
+					break;
+			}
 		}
-	}
-
-	private void ShowWeek()
-	{
-		List<DateTime> days = new List<DateTime>();
-
-		// while (true)
-		// {
-		// 	if (DateTime.Today)
-		// 	{
-		// 		
-		// 	}
-		// }
-		DateTime startDay = DateTime.Today.AddDays(1);
-		 
-		
-		
-		
-		for (int x = 1; x < 7; x++)
-		{
-			days.Add(DateTime.Today.AddDays(x));
-			// Console.WriteLine(days[x]);
-		}
-		
-		// Console.Write(startOfWeek);
 	}
 	
+	// ADD TASK TO JSON FILE
+	private void AddTask()
+	{
+		string date;
+		string title;
+		string description;
+
+		MenuController.SelectDate();
+		date = GetInput(0, "date");
+		MenuController.SelectTitle();
+		title = GetInput(50, "text");
+		MenuController.SelectDescription();
+		description = GetInput(300, "text");
+		
+		SaveTasks(date, title, description);
+	}
+	
+	// PRINT ALL WEEK DAYS WITH TASKS
+	private void ShowWeek()
+	{
+		DateTime startDay;
+		JObject? tasks = ReadTasks();
+
+		if (DaysDiff == 0)
+		{
+			while (true)
+			{
+				if (DateTime.Today.AddDays(DaysDiff).DayOfWeek.ToString() == "Monday")
+				{
+					startDay = DateTime.Today.AddDays(DaysDiff);
+					break;
+				}
+				DaysDiff--;
+			}
+		}
+		else
+		{
+			startDay = DateTime.Today.AddDays(DaysDiff);
+		}
+
+		for (int x = 0; x < 7; x++)
+		{
+			Console.Write(startDay.AddDays(x).ToString(Format) + " [" +  startDay.AddDays(x).DayOfWeek + "]");
+			if (startDay.AddDays(x) == DateTime.Today)
+			{
+				Console.Write(" -> TODAY");
+			}
+			Console.Write("\n");
+
+			if (tasks?[startDay.AddDays(x).ToString(Format)] != null)
+			{
+				foreach (JToken task in tasks[startDay.AddDays(x).ToString(Format)]!)
+				{
+					Console.Write(
+						"   - " + task["Title"] + " (" + task["Description"] + ")\n"
+					);
+				}
+			}
+		}
+	}
+	
+	// READ ALL TASKS
+	private JObject? ReadTasks()
+	{
+		JsonFileExist("tasks.json");
+		
+		string jsonString = File.ReadAllText("tasks.json");
+		dynamic? jsonFile = JsonConvert.DeserializeObject<JObject>(jsonString);
+
+		return jsonFile;
+	}
+
+	// SAVE TO TASKS JSON FILE
+	// date [string] - task date
+	// title [string] - task title
+	// description [description] - task description
+	private void SaveTasks(string date, string title, string description)
+	{
+		JsonFileExist("tasks.json");
+
+		dynamic? tasks = ReadTasks();
+
+		JObject task = new JObject();
+		
+		task.Add("Title", title);
+		task.Add("Description", description);
+		
+		if (tasks?.ContainsKey(date))
+		{
+			tasks?[date].Add(task);
+		}
+		else
+		{
+			JArray taskArray = new JArray() {task};
+			tasks?.Add(date, taskArray);
+		}
+		
+		tasks = JsonConvert.SerializeObject(tasks);
+		File.WriteAllText("tasks.json", tasks);
+	}
+	
+	// CHECK FILE IF EXISTS
+	// FALSE -> CREATE NEW ONE
+	// path [string] - file path
+	public void JsonFileExist(string path)
+	{
+		try
+		{
+			JsonConvert.DeserializeObject(File.ReadAllText(path));
+		}
+		catch
+		{
+			File.Create(path).Close();
+			File.WriteAllText(path, "{}");
+		}
+	}
+	
+	// USER INPUT FUNCTION
+	// maxLetters [int] - max allowed chars
+	// type [string]	- letter (only letters without spaces)
+	//					- text (letters, numbers, spaces)
+	//					- number (only numbers)
+	//					- date (only date)
 	private string GetInput(int maxLetters = 0, string type = "letter")
 	{
 		string input;
@@ -70,6 +180,7 @@ public class Diary
 				input = Console.ReadLine()!;
 				input = input.ToLower();
 
+				// Letters
 				if (type == "letter")
 				{
 					if (Regex.IsMatch(input, @"^[a-zA-Z]+$"))
@@ -90,6 +201,7 @@ public class Diary
 						input = "";
 					}
 				}
+				// Number
 				else if (type == "number")
 				{
 					if (Regex.IsMatch(input, @"^[1234567890]+$"))
@@ -107,6 +219,41 @@ public class Diary
 					else
 					{
 						Console.WriteLine("Only numbers PLS");
+						input = "";
+					}
+				}
+				// Date
+				else if (type == "date")
+				{
+					if (DateTime.TryParse(input, out DateTime parsed))
+					{
+						Console.WriteLine("Enter only " + maxLetters + " letter");
+						input = parsed.ToString(Format);
+					}
+					else
+					{
+						Console.WriteLine("WRITE DATE PLS");
+						input = "";
+					}
+				}
+				// Text
+				else if (type == "text")
+				{
+					if (Regex.IsMatch(input, @"^[a-zA-Z0123456789 ]+$"))
+					{
+						if (!(maxLetters >= input.Length) && maxLetters != 0)
+						{
+							Console.WriteLine("Enter only " + maxLetters + " letter");
+							input = "";
+						}
+						else if (input.Length == 0)
+						{
+							Console.WriteLine("Write something PLS");
+						}    
+					}
+					else
+					{
+						Console.WriteLine("Only letters without special characters PLS");
 						input = "";
 					}
 				}
--- DiaryApp/MenuController.cs
+++ DiaryApp/MenuController.cs
@@ -5,11 +5,37 @@ public class MenuController
 	public void MainMenu()
 	{
 		Console.Write(
-			"\nDIAREK\n" +
-			"----------\n" +
+			"\n[P]REV WEEK  |  [N]EXT WEEK\n\n" +
+			"----------ACTIONS----------\n" +
 			"[A]DD\n" +
 			"[D]ELETE\n" +
-			"[E]XIT\n"
+			"[E]DIT\n" +
+			"[L]EAVE\n" +
+			"---------------------------\n"
+		);
+	}
+	public void SelectDate()
+	{
+		Console.Write(
+			"--------SELECT DATE--------\n" +
+			"FORMAT: month/day/year (12/1/2000)\n" +
+			"---------------------------\n"
+		);
+	}
+	public void SelectTitle()
+	{
+		Console.Write(
+			"--------SELECT TITLE-------\n" +
+			"MAX 50 CHARS\n" +
+			"---------------------------\n"
+		);
+	}
+	public void SelectDescription()
+	{
+		Console.Write(
+			"-----SELECT DESCRIPTION----\n" +
+			"MAX 300 CHARS\n" +
+			"---------------------------\n"
 		);
 	}
 }
